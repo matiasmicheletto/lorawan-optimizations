b269596dc4415db84c5dbf2898e1b8e791e15b19
matias <matiasmicheletto@gmail.com>
2023-07-04 22:32:09 -0300

    Fix greedy

--- bin/src/lib/optimization/greedy.cpp
+++ bin/src/lib/optimization/greedy.cpp
@@ -14,26 +14,27 @@ OptimizationResults greedy(Instance* l, Objective* o, uint maxIters, bool verbos
         sf[i] = 0;
     }
     
-
     std::vector<uint> eds(l->getEDCount());
     std::vector<uint> gws(l->getGWCount());
     std::iota(eds.begin(), eds.end(), 0); // Initialization: 0,1,2,3, ....
     std::iota(gws.begin(), gws.end(), 0); // Initialization: 0,1,2,3, ....
 
     uint loopCounter = 0; // Count number of times the loop is executed (to prevent infinite loop)
-    while(eds.size() > 0) { // While eds to be allocated
+    while(!eds.empty()) { // While eds to be allocated
         // Get index of the GW that can be associated to a larger number of EDs
         uint gwMaxED = 0; // Index of GW with max (initially pointing to the first GW)
         std::vector<uint> maxEdList; // Larger list of EDs associated to a GW
         for(uint j = 0; j < gws.size(); j++) { // For each GW, get attribute (in this case, feasible EDs)
-            double uf = 0.0;
+            double accumulatedUF = 0.0; // UF of current GW
             std::vector<uint> edList; // List of EDs for current GW (initially empty)
-            for(uint i = 0; i < l->getEDCount(); i++){
-                const uint minSF = l->getMinSF(i, j);
-                const uint maxSF = l->getMaxSF(i);
-                if(minSF <= maxSF && uf < 1.0){
-                    edList.push_back(i);
-                    uf += l->getUF(i, minSF); // TODO: using always min SF ?? or max SF ??
+            for(uint i = 0; i < eds.size(); i++){ // For each unallocated ED, check if feasible to current GW
+                uint edToTest = eds.at(i);
+                const uint minSF = l->getMinSF(edToTest, j);
+                const uint maxSF = l->getMaxSF(edToTest);
+                double uf = l->getUF(edToTest, minSF); // TODO: using always min SF ?? or max SF ??
+                if(minSF <= maxSF && (uf+accumulatedUF) < 1.0){
+                    edList.push_back(edToTest);
+                    accumulatedUF += uf; 
                 }
             }
             if(edList.size() > maxEdList.size()){ // If greater than current, update
@@ -44,12 +45,17 @@ OptimizationResults greedy(Instance* l, Objective* o, uint maxIters, bool verbos
         } 
 
         // Remove EDs from list
+        uint removedEDs = 0;
         for(uint i = 0; i < maxEdList.size(); i++){
-            auto edIdx = std::find(eds.begin(), eds.end(), maxEdList.at(i));
-            if (edIdx != eds.end()){
-                gw[maxEdList.at(i)] = gwMaxED;
-                sf[maxEdList.at(i)] = l->getMinSF(maxEdList.at(i), gwMaxED); // TODO: same as before, use min or max??
-                eds.erase(edIdx);
+            for(uint j = 0; j < eds.size(); j++){ // Search for ED index
+                uint edToRemove = maxEdList.at(i);
+                if(eds.at(j) == edToRemove){
+                    gw[edToRemove] = gwMaxED;
+                    sf[edToRemove] = l->getMinSF(edToRemove, gwMaxED); // TODO: same as before, use min or max??
+                    eds.erase(eds.begin()+j);
+                    removedEDs++;
+                    break; // Next ed
+                }
             }
         }
 
