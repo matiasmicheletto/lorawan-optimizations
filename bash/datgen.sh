1ec261880c9174c013ade7368eff500f47a507ca
matias <matiasmicheletto@gmail.com>
2023-08-10 15:56:34 -0300

    Instance generation script

---
+++ bash/datgen.sh
@@ -0,0 +1,8 @@
+
+../bin/datgen -m 500 -e 20000 -g 20 -r 2 -p 0 >> ../dat/ihu_500_20k_20.dat
+../bin/datgen -m 500 -e 20000 -g 20 -r 1 -p 0 >> ../dat/imu_500_20k_20.dat
+../bin/datgen -m 500 -e 20000 -g 20 -r 0 -p 0 >> ../dat/isu_500_20k_20.dat
+
+../bin/datgen -m 500 -e 20000 -g 20 -r 2 -p 1 >> ../dat/ihn_500_20k_20.dat
+../bin/datgen -m 500 -e 20000 -g 20 -r 1 -p 1 >> ../dat/imn_500_20k_20.dat
+../bin/datgen -m 500 -e 20000 -g 20 -r 0 -p 1 >> ../dat/isn_500_20k_20.dat
