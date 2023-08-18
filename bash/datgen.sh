6099602652626481e82ba1dda8dd052cc8091276
matias <matiasmicheletto@gmail.com>
2023-08-18 11:18:45 -0300

    Test case for repeatibility

--- bash/datgen.sh
+++ bash/datgen.sh
@@ -1,8 +1,24 @@
 
-../bin/datgen -m 500 -e 20000 -g 20 -r 2 -p 0 >> ../dat/ihu_500_20k_20.dat
-../bin/datgen -m 500 -e 20000 -g 20 -r 1 -p 0 >> ../dat/imu_500_20k_20.dat
-../bin/datgen -m 500 -e 20000 -g 20 -r 0 -p 0 >> ../dat/isu_500_20k_20.dat
+../bin/datgen -m 100 -g 10 -e 100 -r 1 -p 0 >> ../dat/imu_100_10_100_1.dat
+../bin/datgen -m 100 -g 10 -e 100 -r 1 -p 0 >> ../dat/imu_100_10_100_2.dat
+../bin/datgen -m 100 -g 10 -e 100 -r 1 -p 0 >> ../dat/imu_100_10_100_3.dat
+../bin/datgen -m 100 -g 10 -e 100 -r 1 -p 0 >> ../dat/imu_100_10_100_4.dat
+../bin/datgen -m 100 -g 10 -e 100 -r 1 -p 0 >> ../dat/imu_100_10_100_5.dat
 
-../bin/datgen -m 500 -e 20000 -g 20 -r 2 -p 1 >> ../dat/ihn_500_20k_20.dat
-../bin/datgen -m 500 -e 20000 -g 20 -r 1 -p 1 >> ../dat/imn_500_20k_20.dat
-../bin/datgen -m 500 -e 20000 -g 20 -r 0 -p 1 >> ../dat/isn_500_20k_20.dat
+../bin/datgen -m 100 -g 10 -e 100 -r 1 -p 0 >> ../dat/imu_100_10_100_1.dat
+../bin/datgen -m 100 -g 10 -e 100 -r 1 -p 0 >> ../dat/imu_100_10_100_2.dat
+../bin/datgen -m 100 -g 10 -e 100 -r 1 -p 0 >> ../dat/imu_100_10_100_3.dat
+../bin/datgen -m 100 -g 10 -e 100 -r 1 -p 0 >> ../dat/imu_100_10_100_4.dat
+../bin/datgen -m 100 -g 10 -e 100 -r 1 -p 0 >> ../dat/imu_100_10_100_5.dat
+
+../bin/datgen -m 1000 -g 20 -e 100 -r 1 -p 0 >> ../dat/imu_1000_20_100_1.dat
+../bin/datgen -m 1000 -g 20 -e 100 -r 1 -p 0 >> ../dat/imu_1000_20_100_2.dat
+../bin/datgen -m 1000 -g 20 -e 100 -r 1 -p 0 >> ../dat/imu_1000_20_100_3.dat
+../bin/datgen -m 1000 -g 20 -e 100 -r 1 -p 0 >> ../dat/imu_1000_20_100_4.dat
+../bin/datgen -m 1000 -g 20 -e 100 -r 1 -p 0 >> ../dat/imu_1000_20_100_5.dat
+
+../bin/datgen -m 1000 -g 20 -e 100 -r 1 -p 0 >> ../dat/imu_1000_20_100_1.dat
+../bin/datgen -m 1000 -g 20 -e 100 -r 1 -p 0 >> ../dat/imu_1000_20_100_2.dat
+../bin/datgen -m 1000 -g 20 -e 100 -r 1 -p 0 >> ../dat/imu_1000_20_100_3.dat
+../bin/datgen -m 1000 -g 20 -e 100 -r 1 -p 0 >> ../dat/imu_1000_20_100_4.dat
+../bin/datgen -m 1000 -g 20 -e 100 -r 1 -p 0 >> ../dat/imu_1000_20_100_5.dat
--- bash/gpprs.sh
+++ bash/gpprs.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 
-# Clear the output log before starting
-> ../dat/output_ihu100_10_100.txt
+a_values=(1 1 1)
+b_values=(0.01 1 0.01)
+g_values=(1 1 1000)
 
-# Optimization parameters
-a_values=(1 1 10 1 10 1)
-b_values=(0.01 0.1 0.01 0.01 0.01 0.1)
-g_values=(1 1 1 100 100 100)
-m_values=("GGW" "GE" "GU" "IRS" "GA")
+m_values=("G2")
 
-for i in "${!a_values[@]}"; do
-  for j in "${!m_values[@]}"; do
-    ../bin/gpprs -v \
-      -a "${a_values[i]}" \
-      -b "${b_values[i]}" \
-      -g "${g_values[i]}" \
-      -m "${m_values[j]}" \
-      -f ../dat/ihu100_10_100.dat >> ../dat/output_ihu100_10_100.txt &
+files=(
+"../dat/imu_100_10_100_1.dat"
+"../dat/imu_100_10_100_2.dat"
+"../dat/imu_100_10_100_3.dat"
+"../dat/imu_100_10_100_4.dat"
+"../dat/imu_100_10_100_5.dat"
+"../dat/imu_100_10_100_1.dat"
+"../dat/imu_100_10_100_2.dat"
+"../dat/imu_100_10_100_3.dat"
+"../dat/imu_100_10_100_4.dat"
+"../dat/imu_100_10_100_5.dat"
+"../dat/imu_1000_20_100_1.dat"
+"../dat/imu_1000_20_100_2.dat"
+"../dat/imu_1000_20_100_3.dat"
+"../dat/imu_1000_20_100_4.dat"
+"../dat/imu_1000_20_100_5.dat"
+"../dat/imu_1000_20_100_1.dat"
+"../dat/imu_1000_20_100_2.dat"
+"../dat/imu_1000_20_100_3.dat"
+"../dat/imu_1000_20_100_4.dat"
+"../dat/imu_1000_20_100_5.dat"
+)
 
-    pid=$!
+for k in "${!m_values[@]}"; do
+  for j in "${!files[@]}"; do
+    for i in "${!a_values[@]}"; do
+      ../bin/gpprs -v \
+          -a "${a_values[i]}" \
+          -b "${b_values[i]}" \
+          -g "${g_values[i]}" \
+          -m "${m_values[k]}" \
+          -f "${files[j]}" &
+      pid=$!
 
-    # Wait for the process to finish before starting the next one
-    wait "$pid"
+      wait "$pid"
+    done
   done
 done &
