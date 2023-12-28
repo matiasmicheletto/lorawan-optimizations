2fdf1a5d691c57a5cfbe26bf0a924cec0329379e
matias <matiasmicheletto@gmail.com>
2023-12-28 18:47:44 -0300

    Final stage for final greedy method

--- bin/src/lib/optimization/greedy.cpp
+++ bin/src/lib/optimization/greedy.cpp
@@ -175,19 +175,132 @@ OptimizationResults greedy(Instance* l, Objective* o, uint iters, uint timeout){
     }
 
     OptimizationResults results;
+
     results.cost = feasibleFound ? o->eval(gwBest, sfBest, results.gwUsed, results.energy, results.uf, results.feasible) : __DBL_MAX__;
     results.tp = o->tp;
     results.execTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
-    results.ready = true; // Set export flag to ready
-
     
-    std::cout << "Optimization finished in " << results.execTime << " ms" << std::endl;
+    std::cout << "Stage 2 finished in " << results.execTime << " ms" << std::endl;
     if(feasibleFound){
-        std::cout << "Best result:" << std::endl;
-        o->printSolution(gwBest, sfBest, true, true, true);
+        std::cout << "Best:" << std::endl;
+        o->printSolution(gwBest, sfBest, true, true, false);
     }else{
         std::cout << "No feasible solution was found." << std::endl;
+        return results;
     }
+    
+
+    std::cout << std::endl << "Stage 3 -- Reallocation of ED" << std::endl;
+    
+    uint gwBest2[edCount];
+    uint sfBest2[edCount];
+    std::copy(gwBest, gwBest + edCount, gwBest2);
+    std::copy(sfBest, sfBest + edCount, sfBest2);
+    
+    std::vector<uint> gwList;
+    std::vector<std::vector<uint>> gwEDs(results.gwUsed);
+    std::vector<UtilizationFactor> gwuf; 
+    std::vector<uint>indirection(results.gwUsed);
+    std::iota(indirection.begin(), indirection.end(), 0);
+    for(uint i = 0; i < edCount; i++){
+        UtilizationFactor uf = l->getUF(i, sfBest[i]); // UF of ED i
+        auto it = std::find(gwList.begin(), gwList.end(), gwBest[i]); // Find gw of ED i in list
+        if(it != gwList.end()) { // If found, increase ED and UF
+            uint index = std::distance(gwList.begin(), it);
+            gwEDs[index].push_back(i); // Add ED to GW
+            gwuf[index] += uf;
+        }else{ // If not, add
+            gwList.push_back(gwBest[i]); // Add gw index to list
+            gwEDs[gwList.size()-1].push_back(i); // Add first ED to gw
+            gwuf.push_back(uf); // Add initial UF 
+        }
+    }
+
+    // Sort indirection array in ascending order of number of EDs
+    std::sort(
+        indirection.begin(), 
+        indirection.end(), 
+        [&gwEDs](const uint &a, const uint &b) {
+            return gwEDs[a].size() < gwEDs[b].size();
+        }
+    );
+    
+    // Sort all arrays with indirection array
+    std::vector<uint> sortedGWList(results.gwUsed);
+    std::vector<std::vector<uint>> sortedGWEDs(results.gwUsed);
+    std::vector<UtilizationFactor> sortedgwuf(results.gwUsed);
+    for (uint i = 0; i < results.gwUsed; i++) {
+        sortedGWList[i] = gwList[indirection[i]];
+        sortedgwuf[i] = gwuf[indirection[i]];
+        sortedGWEDs[i] = gwEDs[indirection[i]];
+    }
+
+    for (uint i = 0; i < results.gwUsed; i++) {
+        std::cout << "GW " << sortedGWList[i] << ": " << sortedGWEDs[i].size() << " EDs." << std::endl;
+        for(int s = 7; s <= 12; s++)
+            std::cout << "  UF SF" << s << " = " << sortedgwuf[i].getUFValue(s) << std::endl;
+        std::cout << std::endl;
+    }
+
+    // Start from first GW and try to reallocate all of its EDs to any of the following
+    for (uint g = 0; g < results.gwUsed; g++) {
+        const uint gIndex = sortedGWList[g];
+        uint e = 0;
+        while(e < sortedGWEDs[g].size()) { // For each ED of GW gIndex
+            uint edIndex = sortedGWEDs[g][e]; // Number of ED
+            std::vector<uint> availablesGWs = l->getSortedGWList(edIndex); // List of GW in range of this ED
+            if (availablesGWs.size() > 1){
+                for(uint g2 = 0; g2 < results.gwUsed; g2++) { // Search if possible to allocate to another GW puse el principio de la lista... 
+                    const uint g2Index = sortedGWList[g2];
+                    if(gIndex != g2Index){ 
+                        auto it = std::find(availablesGWs.begin(), availablesGWs.end(), g2Index);
+                        if(it != availablesGWs.end()){ // g2 is in available list, check if enough UF
+                            const uint s = l->getMinSF(edIndex, g2Index); // Use minSF
+                            UtilizationFactor uf = l->getUF(edIndex, s); // UF for this ED using the selected SF
+                            if(!(sortedgwuf[g2] + uf).isFull()) { // This ED can be moved to g2
+                                if (sfBest[edIndex] > s || sortedGWEDs[g].size() == 1){ // If lower SF or remove GW
+                                    std::cout << "Reallocated ED " << edIndex << ": GW " << gIndex << " --> " << g2Index << ", SF: "<<sfBest2[edIndex]<<" --> " << s <<std::endl;	
+                                    gwBest2[edIndex] = g2Index; // Reallocate ED to another GW
+                                    sfBest2[edIndex] = s; // Reallocate ED to new SF
+                                    sortedgwuf[g] -= uf; // Reduce UF of original GW (g)
+                                    sortedGWEDs[g].erase(std::remove(sortedGWEDs[g].begin(), sortedGWEDs[g].end(), edIndex), sortedGWEDs[g].end()); // Remove ED e from GW g
+                                    // Sort again sortedGWList
+                                    std::sort(
+                                        indirection.begin(), 
+                                        indirection.end(), 
+                                        [&sortedGWEDs](const uint &a, const uint &b) {
+                                            return sortedGWEDs[a].size() < sortedGWEDs[b].size();
+                                        }
+                                    );
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            e++;
+        }
+    }
+    
+    OptimizationResults results2;
+    results2.cost = o->eval(gwBest2, sfBest2, results.gwUsed, results.energy, results.uf, results.feasible);
+
+    if(results2.cost < results.cost){
+        std::cout << std::endl << "New optimum: " << results2.cost << " (previous: " << results.cost << ")" << std::endl << std::endl;
+        std::copy(gwBest2, gwBest2 + edCount, gwBest);
+        std::copy(sfBest2, sfBest2 + edCount, sfBest);
+        results = results2;
+    }else
+        std::cout << "No improvement in stage 3. Cost = " << results2.cost << std::endl << std::endl;
+
+    //////////// Export results ////////////
+    results.tp = o->tp;
+    results.execTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
+    results.ready = true;
+    std::cout << "Total exec. time " << results.execTime << " ms" << std::endl;
+    std::cout << "Best:" << std::endl;
+    o->printSolution(gwBest, sfBest, true, true, true);
 
     return results;
 }
