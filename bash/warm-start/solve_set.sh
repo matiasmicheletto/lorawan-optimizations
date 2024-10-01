d345485e5a4e272ec93cef52aa8a83691ffe577c
matias <matiasmicheletto@gmail.com>
2024-10-01 20:06:30 -0300

    Removed greedy runs from warm-start comparison script

--- bash/warm-start/solve_set.sh
+++ bash/warm-start/solve_set.sh
@@ -1,34 +1,7 @@
 
 rm summary.csv
 
-# Greedy
-./greedy -f s_u_100_10_1000.dat -i 50
-./greedy -f s_u_500_10_1000.dat -i 50
-./greedy -f s_u_1000_10_1000.dat -i 50
-./greedy -f s_u_1500_20_1000.dat -i 50
-
-./greedy -f m_u_100_10_1000.dat -i 50
-./greedy -f m_u_500_10_1000.dat -i 50
-./greedy -f m_u_1000_10_1000.dat -i 50
-./greedy -f m_u_1500_20_1000.dat -i 50
-
-./greedy -f h_u_100_10_1000.dat -i 50
-./greedy -f h_u_500_10_1000.dat -i 50
-./greedy -f h_u_1000_10_1000.dat -i 50
-./greedy -f h_u_1500_10_1000.dat -i 50
-
-./greedy -f m_n_100_10_1000.dat -i 50
-./greedy -f m_n_500_10_1000.dat -i 50
-./greedy -f m_n_1000_10_1000.dat -i 50
-./greedy -f m_n_1500_10_1000.dat -i 50
-
-./greedy -f h_n_100_10_1000.dat -i 50
-./greedy -f h_n_500_10_1000.dat -i 50
-./greedy -f h_n_1000_10_1000.dat -i 50
-./greedy -f h_n_1500_10_1000.dat -i 50
-
-
-# GA
+# GA - random
 ./ga -f s_u_100_10_1000.dat -i 100 -t 60
 ./ga -f s_u_500_10_1000.dat -i 100 -t 60
 ./ga -f s_u_1000_10_1000.dat -i 100 -t 60
