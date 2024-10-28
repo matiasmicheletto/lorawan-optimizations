fb1a5e7045b0cd90150fbd55a9414c5cb6dd856e
matias <matiasmicheletto@gmail.com>
2024-10-28 17:33:07 -0300

    Some improvements in moga2 script

--- bash/moga2.sh
+++ bash/moga2.sh
@@ -6,25 +6,26 @@ files=(
 "../dat/imu_100_10_100_5.dat"
 )
 
-# Objective parameter for iteration: should be GW, E, or UF
+# Objective parameter to optimize
 o_values=("GW" "E" "UF")
 
 # Repetitions
 reps=5
 
 # Create file if not exists
-touch ../dat/moga2.sol
+touch moga2.csv
 # Clear file
-> ../dat/moga2.sol
+> moga2.csv
 
 # Run MOGA2
 for i in "${!o_values[@]}"; do
     for j in "${!files[@]}"; do
         for k in $(seq 1 $reps); do
             ../bin/moga2 -f "${files[j]}" -z "${o_values[i]}" -i 10 -q 25 -s 1 -x CSV >> ../dat/moga2.sol &
+            echo "Solving ${files[j]} for objective ${o_values[i]}"
             pid=$!
             wait "$pid"
         done
     done
-done &
-  
+done
+echo "Done"
