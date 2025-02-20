d99c3c31a5843311c27ded6af4d695dcee7d8c65
matias <matiasmicheletto@gmail.com>
2025-02-20 10:11:06 -0300

    Fix pareto MO analysis script

--- bash/pareto_MO.sh
+++ bash/pareto_MO.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 
-inputfile="../dat/Revista/ComparacionModelosHeuristicaHard/generada.dat" # Instance
+#inputfile="../dat/Revista/ComparacionModelosHeuristicaHard/generada.dat" # Instance
+inputfile="../dat/generada.dat" # Instance
 #inputfile="../dat/imu_100_10_100_1.dat" # Instance
+
 
 resultfile="pareto_MO" # Result file
 
 # Crossover methods: "UNIFORM" "SINGLE_POINT" "DOUBLE_POINT"
 cr_method="SINGLE_POINT"
 
-greedy_iters=100 # Number of iterations for Greedy
-moga2_iters=100 # Number of iterations for MOGA2
-moga2_pop=30 # GA population
+greedy_iters=10 # Number of iterations for Greedy
+moga2_iters=10 # Number of iterations for MOGA2
+moga2_pop=20 # GA population
 
 # check if file exists
 if [ ! -f $inputfile ]; then
@@ -35,9 +37,13 @@ for a in 0.0 0.5 1; do
             for c in 0.2 0.4 0.6 0.8; do
                 for m in 0.2 0.4 0.6 0.8; do
                     for i in "${!o_values[@]}"; do
-                        echo "Param values: cr=$c, mut=$m, obj=${o_values[i]}"
-                        echo -n "$a,$b,$g,$c,$m," >> $resultfile.csv
+                        echo "Param values: cr=$c, mut=$m, obj=${o_values[i]}, no custom crossover"
+                        echo -n "$a,$b,$g,$c,$m,no_custom_c," >> $resultfile.csv
                         ../bin/greedy -a $a -b $b -g $g -f $inputfile -i $greedy_iters -p $moga2_pop | ../bin/moga2 -a $a -b $b -g $g -f $inputfile -l $cr_method -z "${o_values[i]}" -c $c -m $m -i $moga2_iters -p -s 1 -x CSV >> $resultfile.csv
+
+                        # echo "Param values: cr=$c, mut=$m, obj=${o_values[i]}, using custom crossover"
+                        #echo -n "$a,$b,$g,$c,$m,custom_c" >> $resultfile.csv
+                        #../bin/greedy -a $a -b $b -g $g -f $inputfile -i $greedy_iters -p $moga2_pop | ../bin/moga2 -a $a -b $b -g $g -f $inputfile -l $cr_method -z "${o_values[i]}" -c $c -m $m -i $moga2_iters -p -s 1 -x CSV >> $resultfile.csv
                     done
                 done 
             done
