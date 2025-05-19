e306c666db882ae0cd7bf5359cdcce4699eecf18
matias <matiasmicheletto@gmail.com>
2025-05-19 15:42:54 -0300

    Fix last script

--- bash/rangos_greedy.sh
+++ bash/rangos_greedy.sh
@@ -9,20 +9,18 @@ if [ ! -f "$inputfile" ]; then
     exit 1
 fi
 
-touch $outputfile.csv
-
-> $outputfile.csv
+> $outputfile
 
 # Run 1000 times
-for i in {1..100}
+for ((i=1; i<=100; i++))
     output=$(../bin/greedy -f "$inputfile" -i 1000 -x)
     exit_status=$? # Error of last command
     if [[ $exit_status -eq 0 ]]; then
-        echo $output >> $outputfile.csv
+        echo "$output" >> "$outputfile"
     else
         echo "Error executing greedy algorithm"
         exit 1
     fi
 done
 
-echo "Execution completed. Results saved in $outputfile.csv"
+echo "Execution completed. Results saved in $outputfile"
