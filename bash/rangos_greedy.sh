32d525c86170488d2f22daaf7a5e1032169aaad1
matias <matiasmicheletto@gmail.com>
2025-05-19 15:47:47 -0300

    Fix last script

--- bash/rangos_greedy.sh
+++ bash/rangos_greedy.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-inputfile="../dat/Revista/ComparacionModelosHeuristicaHard/generada.dat" # Instance
+#inputfile="../dat/Revista/ComparacionModelosHeuristicaHard/generada.dat" # Instance
+#inputfile="../dat/imu_100_10_100_1.dat" # Instance
+inputfile="../dat/instancia.dat" # Instance
 outputfile="promediar.csv" # Result file
 
 # check if file exists
@@ -12,15 +14,16 @@ fi
 > $outputfile
 
 # Run 1000 times
-for ((i=1; i<=100; i++))
+for ((i=1; i<=100; i++)); do
     output=$(../bin/greedy -f "$inputfile" -i 1000 -x)
-    exit_status=$? # Error of last command
+    exit_status=$?
     if [[ $exit_status -eq 0 ]]; then
         echo "$output" >> "$outputfile"
     else
         echo "Error executing greedy algorithm"
         exit 1
     fi
+    echo "Progress: $((i * 100 / 100))%"
 done
 
 echo "Execution completed. Results saved in $outputfile"
